4a8b12af402c318c42fcef26015bb2b802bf7c8e
Arnolnt Spyros <aspyros@it.teithe.gr>
2017-11-17 17:56:43 +0200

    Added few things.Check it!

--- TestApplication/TestApplication/Form1.Designer.cs
+++ TestApplication/TestApplication/Form1.Designer.cs
@@ -28,33 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.mainB = new System.Windows.Forms.Button();
+            this.message1L = new System.Windows.Forms.Label();
+            this.message2L = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // mainB
             // 
-            this.button1.Location = new System.Drawing.Point(66, 96);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(152, 75);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Κουμπί!";
-            this.button1.UseVisualStyleBackColor = true;
+            this.mainB.Location = new System.Drawing.Point(66, 96);
+            this.mainB.Name = "mainB";
+            this.mainB.Size = new System.Drawing.Size(152, 75);
+            this.mainB.TabIndex = 0;
+            this.mainB.Text = "Κουμπί!";
+            this.mainB.UseVisualStyleBackColor = true;
+            this.mainB.Click += new System.EventHandler(this.mainB_Click);
+            // 
+            // message1L
+            // 
+            this.message1L.AutoSize = true;
+            this.message1L.Location = new System.Drawing.Point(18, 217);
+            this.message1L.Name = "message1L";
+            this.message1L.Size = new System.Drawing.Size(0, 13);
+            this.message1L.TabIndex = 1;
+            // 
+            // message2L
+            // 
+            this.message2L.AutoSize = true;
+            this.message2L.Location = new System.Drawing.Point(21, 22);
+            this.message2L.Name = "message2L";
+            this.message2L.Size = new System.Drawing.Size(0, 13);
+            this.message2L.TabIndex = 2;
+            this.message2L.Click += new System.EventHandler(this.message2L_Cick);
             // 
             // TestingForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.message2L);
+            this.Controls.Add(this.message1L);
+            this.Controls.Add(this.mainB);
             this.Name = "TestingForm";
             this.Text = "Δοκιμαστική Φόρμα";
+            this.Load += new System.EventHandler(this.TestingForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button mainB;
+        private System.Windows.Forms.Label message1L;
+        private System.Windows.Forms.Label message2L;
     }
 }
 
--- TestApplication/TestApplication/Form1.cs
+++ TestApplication/TestApplication/Form1.cs
@@ -15,5 +15,29 @@ namespace TestApplication
         {
             InitializeComponent();
         }
+
+        private void message2L_Cick(object sender,EventArgs e)
+        {
+            if(message2L.Text != null)
+            {
+                MessageBox.Show("Hi again");
+                mainB.Enabled = true;
+            }
+           
+        }
+
+
+        private void mainB_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Please remember to work on the same eproject!");
+            mainB.Enabled = false;
+            message1L.Text = "Hello world";
+            message2L.Text = "Hello! It's your project manager Arnold. Click on this text!";
+        }
+
+        private void TestingForm_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
