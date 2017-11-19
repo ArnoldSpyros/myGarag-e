a84374a32cb2ea58e70dc810e5a1af7c4de3e41d
Arnolnt Spyros <aspyros@it.teithe.gr>
2017-11-19 21:05:51 +0200

    "SELECT methods and DELETE methods.Check them."

--- myGarag-e_MAINPROJECT/myGarag-e_MAINPROJECT/DbFiles/DbMethods.cs
+++ myGarag-e_MAINPROJECT/myGarag-e_MAINPROJECT/DbFiles/DbMethods.cs
@@ -35,15 +35,46 @@ namespace myGarag_e_MAINPROJECT.DbFiles
         public static DataSet getTableData(string tableName) // it returns a datased which contains data from a table
         {
             MySqlConnection dbConnection = setMySqlConnection("server=localhost;uid=root;pwd=;database=adopse");
-            string dbCommand = "SELECT * FROM " + tableName;
+            string dbCommand = String.Format("SELECT * FROM {0}", tableName);
             MySqlDataAdapter dataAdapter = new MySqlDataAdapter(dbCommand, dbConnection);
             DataSet dataset = new DataSet();
-
 
             dataAdapter.Fill(dataset, tableName);
             dbConnection.Close();
             return dataset;
 
         }
+
+        public static DataSet getTableData(string tableName, string conditionField, string condition) // it returns a datased which contains data from a table
+        {
+            MySqlConnection dbConnection = setMySqlConnection("server=localhost;uid=root;pwd=;database=adopse");
+            string dbCommand = String.Format("SELECT * FROM {0} WHERE {1}={2}", tableName, conditionField, condition);
+            MySqlDataAdapter dataAdapter = new MySqlDataAdapter(dbCommand, dbConnection);
+            DataSet dataset = new DataSet();
+
+            dataAdapter.Fill(dataset, tableName);
+            dbConnection.Close();
+            return dataset;
+
+        }
+
+
+        public static bool deleteFromTable(string tableName, string conditionField, string condition)
+        {
+            bool deleted = false;
+            MySqlConnection dbConnection = setMySqlConnection("server=localhost;uid=root;pwd=;database=adopse");
+            string dbCommandStr = String.Format("DELETE FROM {0} WHERE {1}={2}", tableName, conditionField, condition);
+            MySqlCommand dbCommand = new MySqlCommand(dbCommandStr, dbConnection);
+
+            if (dbCommand.ExecuteNonQuery() != 0)
+            {
+                deleted = true;
+            }
+
+            dbConnection.Close();
+            return deleted;
+
+        }
+
     }
 }
