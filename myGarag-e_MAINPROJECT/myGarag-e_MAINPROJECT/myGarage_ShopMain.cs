1f2514878b011973c1b041ec3bb71dc822c20ea6
Constantinos Spiridopoulos <lordkarnak@hotmail.co.uk>
2017-11-27 22:20:50 +0200

    Contains 4 new forms (NewClient, NewClientele, NewItem, NewOrder) and changes to button behavior at ShopMain and SystemAdminMain.
    An update to db methods and ShopMain is implemented as well, allowing for exception handling for all methods that call database objects that may not be found.

--- myGarag-e_MAINPROJECT/myGarag-e_MAINPROJECT/myGarage_ShopMain.cs
+++ myGarag-e_MAINPROJECT/myGarag-e_MAINPROJECT/myGarage_ShopMain.cs
@@ -22,32 +22,83 @@ namespace myGarag_e_MAINPROJECT
 
         private void ShopManagementTabs_Selected(object sender, TabControlEventArgs e)
         {
-            MessageBox.Show("You are in the " + ShopManagementTabs.SelectedIndex + " tab"); // This line is for debug purpose. You can remove it if you want to.
-            int idx = ShopManagementTabs.SelectedIndex;
-            DataSet ds;
-
-            switch (idx)
+            try
             {
-                case 0: //
-                    break;
+                MessageBox.Show("You are in the " + ShopManagementTabs.SelectedIndex + " tab"); // This line is for debug purpose. You can remove it if you want to.
+                int idx = ShopManagementTabs.SelectedIndex;
+                DataSet ds;
 
-                case 1: 
-                    ds = DbFiles.DbMethods.getTableData("pelatologio");
-                    dataGridView2.DataSource = ds.Tables["pelatologio"];
-                    break;
+                switch (idx)
+                {
+                    case 0: //
+                        break;
 
-                case 2:
-                    ds = DbFiles.DbMethods.getTableData("proion");
-                    dataGridView3.DataSource = ds.Tables["proion"];
-                    break;
+                    case 1:
+                        ds = DbFiles.DbMethods.getTableData("pelatologio");
+                        dataGridView2.DataSource = ds.Tables["pelatologio"];
+                        break;
+                    case 2:
+                        ds = DbFiles.DbMethods.getTableData("proion");
+                        dataGridView3.DataSource = ds.Tables["proion"];
+                        break;
 
-                case 3: //
-                    break;
+                    case 3: //
+                        break;
 
-                default: //
-                    break;
+                    default: //
+                        break;
+                }
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
 
+        //By defautl this is set to false, it's enabled when a new client form is shown and switches back and forth
+        public static Boolean newClientFormShown = false;
+        private void NewClientBtn_Click(object sender, EventArgs e)
+        {
+            
+            if (newClientFormShown == false)
+            {
+                myGarage_NewClient newclientform = new myGarage_NewClient();
+                newclientform.Show();
+                newClientFormShown = true;
+            }
+        }
+
+        //By defautl this is set to false, it's enabled when a new clientele form is shown and switches back and forth
+        public static Boolean newClienteleFormShown = false;
+        private void NewClienteleBtn_Click(object sender, EventArgs e)
+        {
+            if (newClienteleFormShown == false)
+            {
+                myGarage_NewClientele newclienteleform = new myGarage_NewClientele();
+                newclienteleform.Show();
+                newClienteleFormShown = true;
+            }
+        }
+
+        //By defautl this is set to false, it's enabled when a new item form is shown and switches back and forth
+        public static Boolean newItemFormShown = false;
+        private void AddItemBtn_Click(object sender, EventArgs e)
+        {
+            if (newItemFormShown == false)
+            {
+                myGarage_NewItem newitemform = new myGarage_NewItem();
+                newitemform.Show();
+                newItemFormShown = true;
+            }
+        }
+
+        //By defautl this is set to false, it's enabled when a new order form is shown and switches back and forth
+        public static Boolean newOrderFormShown = false;
+        private void NewOrderBtn_Click(object sender, EventArgs e)
+        {
+            myGarage_NewOrder neworderform = new myGarage_NewOrder();
+            neworderform.Show();
+            newOrderFormShown = true;
         }
     }
 }
