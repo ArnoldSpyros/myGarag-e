a0e037670e68aa80d90da7193b41f0527474bfb1
Arnolnt Spyros <aspyros@it.teithe.gr>
2017-11-30 19:49:10 +0200

    Added some db methods.

---
+++ myGarag-e_MAINPROJECT/myGarag-e_MAINPROJECT/Classes/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myGarag_e_MAINPROJECT.Classes
+{
+    class User
+    {
+        private Minima[] minimata;
+        private RolosUser[] roles = new RolosUser[2];
+        private String username;
+        private String name;
+        private String lastName;
+        private String phoneNumber;
+        private String address;
+        private Autokinito[] autokinita;
+
+        public User()
+        {
+        }
+
+        public User(RolosUser[] roles, string username, string name, string lastName, string phoneNumber, string address)
+        {
+            this.roles = roles;
+            this.username = username;
+            this.name = name;
+            this.lastName = lastName;
+            this.phoneNumber = phoneNumber;
+            this.address = address;
+        }
+
+
+    }
+}
--- myGarag-e_MAINPROJECT/myGarag-e_MAINPROJECT/DbFiles/DbMethods.cs
+++ myGarag-e_MAINPROJECT/myGarag-e_MAINPROJECT/DbFiles/DbMethods.cs
@@ -12,7 +12,7 @@ namespace myGarag_e_MAINPROJECT.DbFiles
     class DbMethods
     {
 
-        public static string connectionString;
+        public static string connectionString = "server=localhost;uid=root;pwd=;database=adopse";
 
         public static MySqlConnection setMySqlConnection(string connectionString)
         {
@@ -48,7 +48,7 @@ namespace myGarag_e_MAINPROJECT.DbFiles
                 dbConnection.Close();
                 return dataset;
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
                 return null;
@@ -58,27 +58,70 @@ namespace myGarag_e_MAINPROJECT.DbFiles
 
         public static DataSet getTableData(string tableName, string conditionField, string condition) // it returns a datased which contains data from a table
         {
-            MySqlConnection dbConnection = setMySqlConnection(connectionString);
-            string dbCommand = String.Format("SELECT * FROM {0} WHERE {1}={2}", tableName, conditionField, condition);
-            MySqlDataAdapter dataAdapter = new MySqlDataAdapter(dbCommand, dbConnection);
-            DataSet dataset = new DataSet();
+            try
+            {
+                MySqlConnection dbConnection = setMySqlConnection(connectionString);
+                string dbCommand = String.Format("SELECT * FROM {0} WHERE {1}={2}", tableName, conditionField, condition);
+                MySqlDataAdapter dataAdapter = new MySqlDataAdapter(dbCommand, dbConnection);
+                DataSet dataset = new DataSet();
 
-            dataAdapter.Fill(dataset, tableName);
-            dbConnection.Close();
-            return dataset;
-
+                dataAdapter.Fill(dataset, tableName);
+                dbConnection.Close();
+                return dataset;
+            }
+            catch (MySqlException obj)
+            {
+                MessageBox.Show(obj.Message);
+                return null;
+            }
         }
 
 
         public static int deleteFromTable(string tableName, string conditionField, string condition)
         {
-            MySqlConnection dbConnection = setMySqlConnection(connectionString);
-            string dbCommandStr = String.Format("DELETE FROM {0} WHERE {1}={2}", tableName, conditionField, condition);
-            MySqlCommand dbCommand = new MySqlCommand(dbCommandStr, dbConnection);
+            try
+            {
+                MySqlConnection dbConnection = setMySqlConnection(connectionString);
+                string dbCommandStr = String.Format("DELETE FROM {0} WHERE {1}={2}", tableName, conditionField, condition);
+                MySqlCommand dbCommand = new MySqlCommand(dbCommandStr, dbConnection);
 
-            int deletedRows = dbCommand.ExecuteNonQuery();
-            dbConnection.Close();
-            return deletedRows; // return the number of deleted rows.
+                int deletedRows = dbCommand.ExecuteNonQuery();
+                dbConnection.Close();
+                return deletedRows; // return the number of deleted rows.
+            }
+            catch (MySqlException obj)
+            {
+                MessageBox.Show(obj.Message);
+                return 0;
+            }
+        }
+
+        public static int registUser(string code, string name, string lastName, string phoneNumber, string password)
+        {
+            
+            try
+            {
+                MySqlConnection dbConnection = setMySqlConnection(connectionString);
+                string dbCommandStr = String.Format("INSERT INTO pelatis (kodikosPelati,onoma,epitheto,tilefono,password)" +
+                    " VALUES (@code,@name,@lastName,@phoneNumber,@password)");
+                MySqlCommand dbCommand = new MySqlCommand(dbCommandStr, dbConnection);
+                dbCommand.Parameters.AddWithValue("@code", code);
+                dbCommand.Parameters.AddWithValue("@name", name);
+                dbCommand.Parameters.AddWithValue("@lastName", lastName);
+                dbCommand.Parameters.AddWithValue("@phoneNumber", phoneNumber);
+                dbCommand.Parameters.AddWithValue("@password", password);
+                int insertedRows = dbCommand.ExecuteNonQuery();
+
+                dbConnection.Close();
+                return insertedRows;
+            }
+            catch (MySqlException obj)
+            {
+                MessageBox.Show(obj.Message);
+                return 0;
+            }
+
+
         }
 
     }
