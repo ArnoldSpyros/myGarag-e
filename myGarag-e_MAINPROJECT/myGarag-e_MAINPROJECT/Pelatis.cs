4cd59d0ab524e426801df090c2a4f9955624d4e6
jou1993 <jou1993@gmail.com>
2017-12-17 20:46:02 +0200

    dhmiourgia ths clashs Pelaths

---
+++ myGarag-e_MAINPROJECT/myGarag-e_MAINPROJECT/Pelatis.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myGarag_e_MAINPROJECT
+{
+    class Pelatis
+    {
+        private ArrayList cart = new ArrayList();
+        private ArrayList rantebou = new ArrayList();
+
+        public void addcart(object item)
+        {
+            cart.Add(item);
+        }
+
+        public void empty()
+        {
+            cart.Clear();
+        }
+
+        public bool isEmpty()
+        {
+            if (cart.Count == 0)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public void aithsiRantebou()
+        {
+
+        }
+
+        public bool cancelRantebou()
+        {
+            return true;
+        }
+        public int num_product()
+        {
+            return cart.Count;
+        }
+
+        public int num_rant()
+        {
+            return 0;
+        }
+    }
+}
