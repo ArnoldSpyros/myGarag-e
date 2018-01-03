3468a6bfa55ada7de6f2a9a093f4d82e313e7684
Arnolnt Spyros <aspyros@it.teithe.gr>
2018-01-03 23:54:19 +0200

    Aaa

---
+++ myGarag-e_MAINPROJECT/myGarag-e_MAINPROJECT/Classes/Katastimatarxis.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myGarag_e_MAINPROJECT.Classes
+{
+    class Katastimatarxis : RolosUser
+    {
+        public Katastimatarxis()
+        {
+            rolosname = "Katastimatarxis";
+        }
+    }
+}
